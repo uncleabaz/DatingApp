83aa9be5510d4af9b6e56dcf324695c0b76fbeef
uncleabaz <stevejobs261@Gmail.com>
2024-05-21 22:46:36 +0000

    End of lesson 2

---
+++ -- SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1 "Tom");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2 "Tim");
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (3 "Tam");
---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,  "TOM");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,  "TIM");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,  "TAME");
